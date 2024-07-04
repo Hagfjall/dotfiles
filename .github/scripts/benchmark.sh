8b26806b971f75a8c625e0f39e5a19c4a0a5121c
Fredrik Hagfjäll <fredrik@hagfjall.se>
2024-07-04 05:58:12 +0200

    wip2

--- .github/scripts/benchmark.sh
+++ .github/scripts/benchmark.sh
@@ -12,10 +12,6 @@ ZSHRC_BENCH=true /tmp/zsh-bench/zsh-bench -i 1 | tee /tmp/zsh-bench.txt
 first_prompt_lag_ms="$(cat /tmp/zsh-bench.txt | grep 'first_prompt_lag_ms' | sed -n 's/.*=\(.*\)/\1/p')"
 first_command_lag_ms="$(cat /tmp/zsh-bench.txt | grep 'first_command_lag_ms' | sed -n 's/.*=\(.*\)/\1/p')"
 
-# neovim
-{ for i in $(seq 1 10); do /usr/bin/time --format="%e" ~/.local/share/zsh/zinit/plugins/neovim---neovim/nvim-linux64/bin/nvim --headless -c "qall"; done; } >/dev/null 2>/tmp/nvim-load-time.txt
-NVIM_LOAD_TIME=$(awk '{ total += $1 } END { print total*1000/NR }' /tmp/nvim-load-time.txt)
-
 # result
 cat <<EOJ | tee /tmp/result-benchmark.json
 [
@@ -28,11 +24,6 @@ cat <<EOJ | tee /tmp/result-benchmark.json
         "name": "zsh first command",
         "unit": "ms",
         "value": ${first_command_lag_ms}
-    },
-    {
-        "name": "neovim load time",
-        "unit": "ms",
-        "value": ${NVIM_LOAD_TIME}
     }
 ]
 EOJ
