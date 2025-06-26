957a84e7494a8a12c8e526d8332ee8367ed7f3d5
Fredrik Hagfjäll <Hagfjall@users.noreply.github.com>
2025-06-26 10:25:54 +0200

    Update aliases.zsh

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -1,3 +1,3 @@
-alias hagfjall='echo hej'
+alias uupdate='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo snap refresh && sudo snap list --all | while read snapname ver rev trk pub notes; do if [[ $notes = *disabled* ]]; then sudo snap remove "$snapname" --revision="$rev"; fi; done'
 alias st='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
