510be6f284573074fb4b406269f06ca61bbe78ca
Fredrik Hagfjäll <fredrik@hagfjall.se>
2025-06-26 06:05:26 +0200

    wip

--- script/installer.sh
+++ script/installer.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+apt_update_ran=false
+install_if_missing() {
+    local cmd="$1"
+    local pkg="${2:-$1}"
+    if ! type -p "$cmd" >/dev/null; then
+        if ! $apt_update_ran; then
+            apt update
+            apt_update_ran=true
+        apt install -y "$pkg"
+    fi
+}
+
+install_if_missing curl
+install_if_missing git
+install_if_missing zsh
 git clone --depth=1 https://github.com/hagfjall/dotfiles.git $HOME/.dotfiles
